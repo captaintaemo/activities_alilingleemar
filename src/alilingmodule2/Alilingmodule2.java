9ddf9231b3c23d88a68c5486b0005cb3a139550f
User <User@CRISQUEL>
2020-11-27 15:29:21 +0700

    MODULE2

---
+++ src/alilingmodule2/Alilingmodule2.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package alilingmodule2;
+import java.util.*;
+/**
+ *
+ * @author User
+ */
+public class Alilingmodule2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Please enter a Name:");
+    Scanner sc = new Scanner(System.in);
+     String name = sc.nextLine();
+     System.out.println("Your name is : "+name);
+    }
+    
+}
